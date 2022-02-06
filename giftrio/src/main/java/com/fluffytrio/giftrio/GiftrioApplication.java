5417b252c3ded0b61576770a1937a53bfbec48e2
limetree-j <limetree.jdh@gmail.com>
2022-02-06 18:34:28 +0900

    Initial commit

---
+++ giftrio/src/main/java/com/fluffytrio/giftrio/GiftrioApplication.java
@@ -0,0 +1,13 @@
+package com.fluffytrio.giftrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftrioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftrioApplication.class, args);
+	}
+
+}
---
+++ giftrio/src/test/java/com/fluffytrio/giftrio/GiftrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluffytrio.giftrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
