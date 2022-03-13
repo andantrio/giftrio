4a27166ec195358182133e3b3017557582509029
jisu <sh8532k@naver.com>
2022-03-13 19:24:52 +0900

    [ADD] Create AdventRepository

--- giftrio/src/main/java/com/fluffytrio/giftrio/advent/Advent.java
+++ giftrio/src/main/java/com/fluffytrio/giftrio/advent/Advent.java
@@ -2,6 +2,9 @@ package com.fluffytrio.giftrio.advent;
 
 import com.fluffytrio.giftrio.calendar.Calendar;
 import com.fluffytrio.giftrio.users.Users;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,13 +13,14 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor
 @Entity
-@MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class Advent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name="user_id", nullable = false, updatable = false)
@@ -29,7 +33,7 @@ public class Advent {
     private int seqNum;
     private LocalDate adventDate;
 
-    @Column(nullable = false)
+    @Column(length = 128, nullable = false)
     private String text;
     private String img;
 
@@ -42,4 +46,13 @@ public class Advent {
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @Builder
+    public Advent(Users users, Calendar calendar, LocalDate adventDate, String text) {
+        this.userId = users;
+        this.calendarId = calendar;
+        this.adventDate = adventDate;
+        this.text = text;
+        this.isOpen = false;
+    }
 }
