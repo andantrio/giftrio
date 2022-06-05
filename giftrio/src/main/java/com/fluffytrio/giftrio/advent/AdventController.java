0c6109815839665e38c9d6617791e646564ad726
2jisu <sh8532k@naver.com>
2022-06-05 18:44:26 +0900

    [ADD] Add getAdvent, getAdvents, patchAdvent, deleteAdvent

--- giftrio/src/main/java/com/fluffytrio/giftrio/advent/AdventController.java
+++ giftrio/src/main/java/com/fluffytrio/giftrio/advent/AdventController.java
@@ -2,21 +2,40 @@ package com.fluffytrio.giftrio.advent;
 
 import com.fluffytrio.giftrio.advent.dto.AdventRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/advent")
+@RequestMapping("/api/v1/advents")
 public class AdventController {
 
     private final AdventService adventService;
+
+    @GetMapping("/{adventId}")
+    public Optional<Advent> getAdvent(@PathVariable Long adventId){
+        return adventService.getAdvent(adventId);
+    }
+
+    @GetMapping()
+    public List<Advent> getAdvents(){
+        return adventService.getAdvents();
+    }
 
     @PostMapping()
     public Advent addAdvent(@RequestBody AdventRequestDto adventRequestDto){
         return adventService.addAdvent(adventRequestDto);
     }
 
+    @PutMapping("/{adventId}")
+    public Advent patchAdvent(@RequestBody AdventRequestDto adventRequestDto){
+        return adventService.patchAdvent(adventRequestDto);
+    }
+
+    @DeleteMapping("/{adventId}")
+    public boolean deleteAdvent(@PathVariable Long adventId){
+        return adventService.deleteAdvent(adventId);
+    }
 }
--- giftrio/src/main/java/com/fluffytrio/giftrio/advent/AdventService.java
+++ giftrio/src/main/java/com/fluffytrio/giftrio/advent/AdventService.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class AdventService {
@@ -14,5 +17,35 @@ public class AdventService {
     @Transactional
     public Advent addAdvent(AdventRequestDto adventRequestDto){
         return adventRepository.save(adventRequestDto.toEntity());
+    }
+
+    @Transactional
+    public Optional<Advent> getAdvent(Long adventId){
+        return adventRepository.findById(adventId);
+    }
+
+    @Transactional
+    public List<Advent> getAdvents(){
+        return adventRepository.findAll();
+    }
+
+    @Transactional
+    public Advent patchAdvent(AdventRequestDto adventRequestDto){
+        Optional<Advent> originAdvent = adventRepository.findById(adventRequestDto.toEntity().getId());
+//        if(! originAdvent.isPresent()){
+//            return Exception;
+//        }
+        return adventRepository.save(adventRequestDto.toEntity());
+    }
+
+    @Transactional
+    public boolean deleteAdvent(Long adventId){
+        Optional<Advent> advent = adventRepository.findById(adventId);
+        if(advent.isPresent()){
+            advent.get().delete();
+            adventRepository.save(advent.get());
+            return true;
+        }
+        return false;
     }
 }
