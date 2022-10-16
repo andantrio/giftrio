2d38caa9bb9668c4d5d52ab83a3043bb9748839c
2jisu <jisu.jjang88@gmail.com>
2022-10-16 18:08:02 +0900

    [MODIFY] request 타입 수정

--- giftrio/src/main/java/com/fluffytrio/giftrio/file/FileController.java
+++ giftrio/src/main/java/com/fluffytrio/giftrio/file/FileController.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,7 +17,7 @@ import com.fluffytrio.giftrio.utils.file.FileUpload;
 public class FileController {
 
     @PostMapping()
-    public String addFile(@RequestBody MultipartFile multipartFile) throws IOException {
+    public String addFile(@RequestParam("file") MultipartFile multipartFile) throws IOException {
         return FileUpload.saveFile(multipartFile);
     }
 
